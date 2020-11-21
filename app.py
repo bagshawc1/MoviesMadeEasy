c55ebe6a6d79b7d0682f5bb61e05e03e83b49546
Shehzer <snaumani@uwo.ca>
2020-11-21 01:01:02 -0500

    @backend - simple connection

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask import jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify({'text': 'Hello World!'})
+
+
+if __name__ == '__main__':
+     app.run(port=5002)
